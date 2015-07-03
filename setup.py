d9413eb825b7d2dcb625d6f9e9fc398e47c19b01
William Schoenell <wschoenell@gmail.com>
2015-07-03 15:32:02 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='chimera-serial',
+    name='chimera-optec',
     version='0.0.1',
-    packages=['chimera_serial', 'chimera_serial.instruments'],
+    packages=['chimera_optec', 'chimera_optec.instruments'],
     scripts=[],
-    url='http://github.com/astroufsc/chimera-serial',
+    url='http://github.com/astroufsc/chimera-optec',
     license='GPL v2',
     author='William Schoenell',
     author_email='william@iaa.es',
     install_requires=['pyserial'],
-    description='Chimera plugin for serial devices'
+    description='Chimera plugin for OPTEC focusers'
 )
